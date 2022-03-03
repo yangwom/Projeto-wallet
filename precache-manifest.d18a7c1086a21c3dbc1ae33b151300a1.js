68c821259740c16bccd79d93a9362b88c35aa2fd
yang vieira <yangflix3@gmail.com>
2022-03-02 23:12:53 -0400

    Updates

---
+++ precache-manifest.d18a7c1086a21c3dbc1ae33b151300a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b433a3f953f4633b93c7e26831acdeb",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "d5ebdf7d81ee0749079d",
+    "url": "/app-toot-name/static/css/main.d09563ee.chunk.css"
+  },
+  {
+    "revision": "713a0832a0fe294fcb56",
+    "url": "/app-toot-name/static/js/2.deb961a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-toot-name/static/js/2.deb961a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ebdf7d81ee0749079d",
+    "url": "/app-toot-name/static/js/main.9f7ae417.chunk.js"
+  },
+  {
+    "revision": "66ac581fbc8990623b1d",
+    "url": "/app-toot-name/static/js/runtime-main.cfc30c8d.js"
+  }
+]);
